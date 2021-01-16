0bc3d99cc23ff8d4b4b891ab7cf79c8121022552
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-01-16 17:44:05 +0100

    validation error array of strings / not array obj.

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
@@ -5,6 +6,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -28,7 +30,24 @@ namespace API
       services.AddAutoMapper(typeof(MappingProfiles));
       services.AddControllers();
       services.AddDbContext<StoreContext>(x =>
-    x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+        x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+      services.Configure<ApiBehaviorOptions>(options =>
+      {
+        options.InvalidModelStateResponseFactory = actionContext =>
+        {
+          var errors = actionContext.ModelState
+            .Where(e => e.Value.Errors.Count > 0)
+            .SelectMany(x => x.Value.Errors)
+            .Select(x => x.ErrorMessage).ToArray();
+
+          var errorResponse = new Errors.ApiValidationErrorResponse
+          {
+            Errors = errors
+          };
+
+          return new BadRequestObjectResult(errorResponse);
+        };
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
