36d63fd19753e406499e595ead1eab19511ed155
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-01-16 18:07:47 +0100

    Add swagger for documenting API

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -48,6 +49,11 @@ namespace API
           return new BadRequestObjectResult(errorResponse);
         };
       });
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkyNet API", Version = "v1" });
+      });
+
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,6 +70,9 @@ namespace API
       app.UseStaticFiles();
 
       app.UseAuthorization();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkyNet API v1"); });
 
       app.UseEndpoints(endpoints =>
       {
