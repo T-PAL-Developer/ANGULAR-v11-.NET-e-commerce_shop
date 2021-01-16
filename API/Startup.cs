e8a8f1dbff2ae75d76fe427656de880aee7aa6ef
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-01-16 18:52:38 +0100

    Cleaning the Startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,17 +1,13 @@
-using System.Linq;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -26,33 +22,14 @@ namespace API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddScoped<IProductRepository, ProductRepository>();
-      services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
       services.AddAutoMapper(typeof(MappingProfiles));
       services.AddControllers();
       services.AddDbContext<StoreContext>(x =>
         x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-      services.Configure<ApiBehaviorOptions>(options =>
-      {
-        options.InvalidModelStateResponseFactory = actionContext =>
-        {
-          var errors = actionContext.ModelState
-            .Where(e => e.Value.Errors.Count > 0)
-            .SelectMany(x => x.Value.Errors)
-            .Select(x => x.ErrorMessage).ToArray();
 
-          var errorResponse = new Errors.ApiValidationErrorResponse
-          {
-            Errors = errors
-          };
-
-          return new BadRequestObjectResult(errorResponse);
-        };
-      });
-      services.AddSwaggerGen(c =>
-      {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkyNet API", Version = "v1" });
-      });
+      services.AddApplicationServices();
+      services.AddSwaggerDocumentation();
 
     }
 
@@ -71,8 +48,7 @@ namespace API
 
       app.UseAuthorization();
 
-      app.UseSwagger();
-      app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkyNet API v1"); });
+      app.UseSwaggerDocumention();
 
       app.UseEndpoints(endpoints =>
       {
