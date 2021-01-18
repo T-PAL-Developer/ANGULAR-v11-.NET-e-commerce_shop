f05ba3b78a7cacf067d12f341def3b4ea1773298
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-01-18 22:22:18 +0100

    create interface brands types

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IType } from '../shared/models/productType';
 
 @Injectable({
   providedIn: 'root'
@@ -14,4 +16,12 @@ export class ShopService {
   getProducts(): Observable<IPagination> {
     return this.http.get<IPagination>(this.baseUrl + 'products?PageSize=50');
   }
+  getBrands(): Observable<IBrand[]> {
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+  }
+  getTypes(): Observable<IType[]> {
+    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+  }
+
 }
+
